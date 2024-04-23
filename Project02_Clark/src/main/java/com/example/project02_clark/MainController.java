b38984df8faa21006fa7f7b00dc100950e9260b1
amillionhornets <dylan.j.clark319@gmail.com>
2024-04-23 15:23:11 -0500

    Fixed Main Controller

--- Project02_Clark/src/main/java/com/example/project02_clark/MainController.java
+++ Project02_Clark/src/main/java/com/example/project02_clark/MainController.java
@@ -15,7 +15,7 @@ public class MainController {
     // Method to open the signup window
     public void signup() {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Signup.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("StudentSignup.fxml"));
             Stage stage = new Stage();
             stage.setScene(new Scene(loader.load()));
             stage.show();
@@ -27,7 +27,7 @@ public class MainController {
     // Method to open the login window
     public void login() {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("StudentLogin.fxml"));
             Stage stage = new Stage();
             stage.setScene(new Scene(loader.load()));
             stage.show();
